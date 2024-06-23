c1d5acc38ea47b7efb314ea9bc3e1d719211fda3
silver-lining0504 <39733779+silver-lining0504@users.noreply.github.com>
2024-06-23 17:35:04 +0900

    改行を変換するようにした index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,15 @@
 <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet"></link>
-<style></style>
 <template>
   <div>
     <h1 class="text-4xl font-bold mb-8">Blog</h1>
     <div v-if="blogs.length">
-      <ul class="space-y-4">
-        <li v-for="blog in blogs" :key="blog.id" class="p-4 border rounded-lg shadow-sm">
+      <ul class="space-y-4 px-4">
+        <li v-for="blog in blogs" :key="blog.id" class="p-4 border rounded-lg shadow-sm bg-slate-100">
           <h1><nuxt-link :to="'/blog/' + blog.id" class="text-2xl text-blue-600 hover:underline">
             {{ blog.title }}
           </nuxt-link></h1>
           <br>{{ blog.summary }}
-          <br>{{ blog.content }}
+          <div v-html="convertPToBr(blog.content)"></div>
           <img :src="blog.thumbnail.url" class="size-4/12">
         </li>
       </ul>
@@ -20,14 +19,18 @@
     </div>
   </div>
 </template>
-
 <script>
-
 export default {
   data() {
     return {
       blogs: []
     };
+  },
+  methods: {
+    convertPToBr(content) {
+      // <p> タグを改行に変換
+      return content.replace(/<\/p>/g, '<br>').replace(/<p>/g, '');
+    }
   },
   async fetch() {
     const { data } = await this.$axios.get('/blog', {
